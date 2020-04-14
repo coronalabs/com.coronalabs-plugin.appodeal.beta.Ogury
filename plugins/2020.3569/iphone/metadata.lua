a8f78b762b98955239ee09ea99a2d322cfe9c096
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:02:25 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDOguryAdapter', },
+		frameworks = { 'OMSDK_Oguryco', 'OguryAds', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
